337a953202af5bee5bae2e990fec6f63cc2879d0
zhumanyao <zhumanyao@sungrowpower.com>
2026-05-25 16:23:50 +0800

    fix: 优化名称

--- src/transport/http.ts
+++ src/transport/http.ts
@@ -3,7 +3,7 @@
  * @Date: 2026-05-22 21:13:13
  * @LastEditors: zhumanyao zhumanyao@sungrowpower.com
  * @LastEditTime: 2026-05-25 09:30:28
- * @FilePath: \read-gitlabl\src\transport\http.ts
+ * @FilePath: \read-gitl\src\transport\http.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 /**
